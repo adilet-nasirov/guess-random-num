01dc491cafc6c1dda6799f49b1e0479e5bcfd3d8
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-02-11 23:58:15 -0800

    added stylings, functions that will tell win/lost

--- src/App.js
+++ src/App.js
@@ -6,28 +6,73 @@ class App extends React.Component {
     super();
     this.state = {
       first: 0,
-      second: 0,
+      second: 10,
       attempt: 3,
-      guess: 0,
-      random: 0,
+      guessNum: undefined,
+      clues: false,
+      random: null,
+      guess: true,
+      ifWon: false,
+      hints: "",
     };
   }
   handleInput = (e) => {
-    let target = e.target;
-    this.setState({ [target.name]: +target.value });
+    // if target is checkbox change to ischecked, else get value;
+    let target =
+      e.target.type === "checkbox" ? e.target.checked : +e.target.value;
+    //change state of the particular elements value
+    this.setState({ [e.target.name]: target });
   };
   generateRandomNum = () => {
-    const { first, second, random } = this.state;
+    const { first, second } = this.state;
     if (first < second) {
+      //generate random number between first and second nums
       let randomNum = Math.floor(Math.random() * (second - first + 1) + first);
-      console.log(randomNum);
+      // save it on state;
       this.setState({ random: randomNum });
     }
   };
   guessHandler = () => {
-    const { guess, random } = this.state;
-    if (guess === random) {
+    //destructuring vars from state
+    const { guessNum, random, attempt, clues } = this.state;
+    //if user guesses right
+    if (guessNum === random) {
       console.log("You gotcha", random);
+      setTimeout(() => {
+        this.setState({ guess: true, ifWon: true });
+        setTimeout(() => {
+          // this.setState({ ifWon: false });
+          let play = window.confirm("Do you want to play again?");
+          if (play === true) {
+            // re-renders page if user wants again
+            window.location.reload(true);
+          } else {
+            // if user doesn't want to play again web page closes
+            window.close();
+          }
+        }, 2000);
+      }, 100);
+    } else if (attempt <= 1) {
+      this.setState({ attempt: 0 });
+      setTimeout(() => {
+        window.confirm(`Sorry, you lost! It was **${random}**`);
+        window.location.reload(true);
+      }, 500);
+    } else {
+      this.setState({ attempt: this.state.attempt - 1, ifWon: false });
+      setTimeout(() => {
+        this.setState({ guess: false });
+        setTimeout(() => {
+          this.setState({ guess: true });
+        }, 2000);
+      }, 100);
+      if (this.state.clues && this.state.guessNum !== undefined) {
+        guessNum > random
+          ? this.setState({ hints: "Smaller" })
+          : this.setState({ hints: "Bigger" });
+      }
+
+      console.log("false");
     }
   };
   render() {
@@ -38,8 +83,24 @@ class App extends React.Component {
           <p>{this.state.attempt} attempts left</p>
         </div>
         <div className="container">
-          <div className="modal" style={{ display: "none" }}>
-            <p>Sorry your guess didn't match! Try again</p>
+          <div
+            className="modal"
+            style={
+              !this.state.guess ? { display: "block" } : { display: "none" }
+            }
+          >
+            <p style={{ color: "red" }}>
+              Sorry your guess didn't match! Try again
+            </p>
+          </div>
+
+          <div
+            className="winModal"
+            style={
+              this.state.ifWon ? { display: "block" } : { display: "none" }
+            }
+          >
+            <p>You won ! ! ! Yeeeeey</p>
           </div>
           <div className="inputDiv">
             <label>From</label>
@@ -64,13 +125,18 @@ class App extends React.Component {
           </div>
           <div className="guess">
             <p className="pForGuess">Guess the number</p>
-            <div className="hintParagraph" style={{ display: "none" }}>
-              <p id="clue">upper</p>
+            <div
+              className="hintParagraph"
+              style={
+                this.state.clues ? { display: "block" } : { display: "none" }
+              }
+            >
+              <p id="clue">{this.state.hints}</p>
             </div>
             <input
               type="number"
               onChange={this.handleInput}
-              name="guess"
+              name="guessNum"
               id="guessInput"
             />
             <button className="submit" onClick={this.guessHandler}>
@@ -79,7 +145,12 @@ class App extends React.Component {
           </div>
         </div>
         <div className="hintClick">
-          <input type="checkbox" id="hint" />
+          <input
+            type="checkbox"
+            name="clues"
+            id="hint"
+            onChange={this.handleInput}
+          />
           <label htmlFor="hint"> With clues</label>
         </div>
       </div>
